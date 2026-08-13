e9abdcf33e53bea9ff5730d1cded993ba41e9eca
rupontinova <rupontibup@gmail.com>
2026-08-13 12:45:57 +0600

    Revert GitHub Pages changes for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/rupontis_portfolio",
-  images: {
-    unoptimized: true,
-  },
   allowedDevOrigins: ['192.168.1.5'],
 };
 
